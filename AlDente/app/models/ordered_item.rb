974ba2eedd16399b28b1a8c43c58a4edac4c7fa3
Harris Hartung <hjh@andrew.cmu.edu>
2014-10-27 21:27:17 -0400

    pulled from master

--- AlDente/app/models/ordered_item.rb
+++ AlDente/app/models/ordered_item.rb
@@ -3,4 +3,10 @@ class OrderedItem < ActiveRecord::Base
 	#relationships
 	belongs_to :item_split
 	belongs_to :customer
+
+	#validations
+	validates_presence_of :item_split_id
+	validates :item_split_id, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
+	validates_presence_of :customer_id
+	validates :customer_id, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
 end
