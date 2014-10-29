9fe38b97a214a4d850d26151a0517e6e00d16ef2
connie chen <conniechen2154@gmail.com>
2014-10-29 17:51:52 -0400

    session model edits

--- AlDente/app/models/session.rb
+++ AlDente/app/models/session.rb
@@ -1,11 +1,22 @@
 class Session < ActiveRecord::Base
 	belongs_to :restaurant
-    has_many :customers
+    has_many :ordered_items #is this necessary since its an optional relationship
 
 	validates_presence_of :waiter_name
 	validates_presence_of :ipad_id
 	validates_presence_of :restaurant_id
 	validates_presence_of :user_id
-	
+	validates_presence_of :PIN
+	validates_presence_of :party_size
+	validates :party_size { only_integer: true }
+
 	scope :alphabetically, order('waiter_name')
-end
+
+
+	def createpin (length = 6)
+		chars = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789'
+  		p = ''
+  		length.times { p << chars[rand(chars.size)] }
+  		p
+	end
+end	
