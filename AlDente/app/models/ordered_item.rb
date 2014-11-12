381756e143b6785cc41ef7079b83ace6182d3847
Harris Hartung <hjh@andrew.cmu.edu>
2014-11-12 18:13:49 -0500

    integrating users into models, added bcrypt to gemfile

--- AlDente/app/models/ordered_item.rb
+++ AlDente/app/models/ordered_item.rb
@@ -8,4 +8,7 @@ class OrderedItem < ActiveRecord::Base
 	validates :session_id, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
 	validates_presence_of :item_id
 	validates :item_id, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
+
+	#scope
+	scope :by_session where('session_id = ?', :session_id)
 end
--- AlDente/app/models/restaurant.rb
+++ AlDente/app/models/restaurant.rb
@@ -3,6 +3,7 @@ class Restaurant < ActiveRecord::Base
 	has_many :items
 	has_many :sessions
 	has_many :customers
+	has_many :users
     
 	validates_presence_of :name
 	
