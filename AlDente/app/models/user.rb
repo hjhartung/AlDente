f49eb243188bab9a102cb5a4ab1c88e5d6245505
Harris Hartung <hjh@andrew.cmu.edu>
2014-11-17 22:19:14 -0500

    working on adding users and sessions

--- AlDente/app/models/user.rb
+++ AlDente/app/models/user.rb
@@ -3,9 +3,14 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	validates_presence_of :restaurant_id
+	validates_uniqueness_of :restaurant_id
 	validates_inclusion_of :role, :in => %w[admin member], :message => "is not recognized by the system"
 
   def self.authenticate(restaurant_id, password)
     find(restaurant_id).try(:authenticate, password)
+  end
+
+  def self.admin?
+  	return self.role == 'admin'
   end
 end
